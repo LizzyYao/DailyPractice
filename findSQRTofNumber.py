9f03711b84ce03679cc33d3f3f2a07f10dd8d43d
Lizzy Yao <lizzyyao11@gmail.com>
2015-06-08 23:39:39 -0400

    push SQRT code

---
+++ findSQRTofNumber.py
@@ -0,0 +1,6 @@
+import math
+#find squareroot of a given positive number, sorry I don't know what is complex number.
+number=input("Please enter a positive number:")
+number=float(number)
+sqrt=math.sqrt(number)
+print("the squareroot of {} is {}".format(number,sqrt))
