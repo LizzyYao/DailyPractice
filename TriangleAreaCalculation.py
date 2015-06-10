369636d6cacff00f11cc693cfb946972c21b8475
Lizzy Yao <lizzyyao11@gmail.com>
2015-06-09 21:40:47 -0400

    push area of triangle

---
+++ TriangleAreaCalculation.py
@@ -0,0 +1,11 @@
+import math
+a=float(input("please enter the first side: "))
+b=float(input("please enter the second side: "))
+c=float(input("please enter the third side: "))
+p=(a+b+c)/2
+#use Heron's formula to get the area of the triangle,
+#I can only understand this formula,
+#It's time-consuming for me to figure out other formulas.
+S=math.sqrt(p*(p-a)*(p-b)*(p-c))
+print "the area of this triangle you entered is: ",S
+
