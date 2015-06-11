951222a82f766ed650bb053c1fb8277a43a7b4a5
Lizzy Yao <lizzyyao11@gmail.com>
2015-06-10 21:51:17 -0400

    modify

--- TriangleAreaCalculation.py
+++ TriangleAreaCalculation.py
@@ -1,11 +1,11 @@
 import math
-a=float(input("please enter the first side: "))
-b=float(input("please enter the second side: "))
-c=float(input("please enter the third side: "))
-p=(a+b+c)/2
+triangleSideA=float(input("please enter the length of first side(cm): "))
+triangleSideB=float(input("please enter the length of second side(cm): "))
+triangleSideC=float(input("please enter the length of third side(cm): "))
+parameter=(triangleSideA+triangleSideB+triangleSideC)/2
 #use Heron's formula to get the area of the triangle,
 #I can only understand this formula,
 #It's time-consuming for me to figure out other formulas.
-S=round(math.sqrt(p*(p-a)*(p-b)*(p-c)),2)
-print "the area of this triangle you entered is: ",S
+area=round(math.sqrt(parameter*(parameter-triangleSideA)*(parameter-triangleSideB)*(parameter-triangleSideC)),2)
+print "the area of this triangle you entered is: ",area
 
