b975a32d4b19314424ea7ca71350b3a7e417db7d
Lizzy Yao <lizzyyao11@gmail.com>
2015-06-10 21:05:46 -0400

    push my magic 2 values exchange

---
+++ exchange2Values.py
@@ -0,0 +1,10 @@
+valueA=raw_input("Please enter value A: ")
+valueB=raw_input("Please enter value B: ")
+#code below to switch the 2 values to make a magic
+tempValue=valueA
+valueA=valueB
+valueB=tempValue
+print"Please see our MAGIC"
+print"Your MAGIC value A now is: ", valueA
+print"Your MAGIC value B now is: ", valueB
+print"lalalalala! hahahaha!"
