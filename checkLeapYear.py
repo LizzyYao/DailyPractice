70118aa894ddcc053718733fb448b6749aaa4a1b
Lizzy Yao <lizzyyao11@gmail.com>
2015-06-13 23:03:16 -0400

    leap year older version view

--- checkLeapYear.py
+++ checkLeapYear.py
@@ -1,5 +1,8 @@
 year=int(input("Please enter year you would like to check: "))
-if (year % 100 == 0) and (year % 400 != 0) or (year % 4 != 0): 
-	print ("This year {} is not leap year.".format(year))
+if year%4==0:
+	if year%100==0 and year%400!=0:
+		print ("This year {} is not leap year.".format(year))
+	else:
+		print ("This year {} is leap year.".format(year))
 else:
-	print ("This year {} is leap year.".format(year))
+	print ("This year {} is not leap year.".format(year))
