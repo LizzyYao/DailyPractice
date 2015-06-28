4c0d180cabec466e117a4693a991a4f8d1ca5e51
Lizzy Yao <lizzyyao11@gmail.com>
2015-06-28 14:54:45 -0400

    to pull all python re-orginazied code to local folder

--- findLargestNumber.py
+++ findLargestNumber.py
@@ -1,4 +1,5 @@
 #find largest number in 3 numbers.
+
 number1=input("Please enter 1st number: ")
 number2=input("Please enter 2nd number: ")
 number3=input("Please enter 3rd number: ")
@@ -6,15 +7,30 @@ number3=input("Please enter 3rd number: ")
 number1=float(number1)
 number2=float(number2)
 number3=float(number3)
-#find largest number.
-if (number1>number2) and (number1>number3):
-    largestNumber=number1
-elif (number2>number1) and (number2>number3):
-    largestNumber=number2
+
+
+#find largest number
+def getNumbers(number1,number2,number3):
+    print number1,number2,number3
+    return number1,number2,number3
+getNumbers(number1,number2,number3)
+
+if number1>number2 and number1>number3:
+	largestNumber=number1
+elif number2>number3 and number2>number1:
+	largestNumber=number2
 else:
-    largestNumber=number3
+	largestNumber=number3
 
-def printLargestNumber(largestNumber):
-    print ("The largest number among these three is: ", largestNumber)
+def findLargestNumber(largestNumber):
+	print largestNumber
+	return largestNumber
+findLargestNumber(largestNumber)
 
-printLargestNumber(largestNumber)
+#output largest number
+def printLargestNumber():
+    print "The numbers you entered are {} {} and{}, ".format(number1,number2,number3)
+    print "The largest number among these numbers is: {}".format(largestNumber)
+
+printLargestNumber()
+
