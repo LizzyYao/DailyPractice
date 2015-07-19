99a6ea7140da62b0984d7a6b896f3c3f5ff869a6
Lizzy Yao <lizzyyao11@gmail.com>
2015-07-19 18:37:34 -0400

    repush findLargestNumber code

--- DailyPractice/13_findLargestNumber.py
+++ DailyPractice/13_findLargestNumber.py
@@ -1,29 +1,19 @@
 #find largest number in 3 numbers.
 
-<<<<<<< HEAD
-#get the numbers from user and convert string to float numbers.
-
-def NumberFromUser(ordinal):
-=======
-def userInput(ordinal):
-	"get the numbers from user and convert string to float numbers."
->>>>>>> ab5afbef6c9fdfb015abb99478b9d11d754d8670
+def getNumberFromUser(ordinal):
+#"get the numbers from user and convert string to float numbers."
 	number=raw_input("please enter " + ordinal + " number: ")
 	number=float(number)
 	return number
 	
-number1=NumberFromUser("first")
-number2=NumberFromUser("second")
-number3=NumberFromUser("third")
+number1=getNumberFromUser("first")
+number2=getNumberFromUser("second")
+number3=getNumberFromUser("third")
 
-<<<<<<< HEAD
-def FindLargestNumber():
+
+def findLargestNumber():
 #evaluate each statement to find the largest number.
-=======
 
-def process():
-	"evaluate each statement to find the largest number."
->>>>>>> ab5afbef6c9fdfb015abb99478b9d11d754d8670
 	if number1>number2 and number1>number3:
 		largestNumber=number1
 	elif number2>number3 and number2>number1:
@@ -32,13 +22,13 @@ def process():
 		largestNumber=number3
 	return largestNumber
 		
-largestNumber = FindLargestNumber()
+largestNumber = findLargestNumber()
 	
-def PrintLargestNumber(largestNumber):
+def printLargestNumber(largestNumber):
 	print("The largest number in {} {} and {} is: {}".format(number1,number2,number3,largestNumber))
 
 
-PrintLargestNumber(largestNumber)
+printLargestNumber(largestNumber)
 
 
 
