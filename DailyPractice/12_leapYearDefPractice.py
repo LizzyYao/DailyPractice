6b964a9c060509e9fab0570aaae533931ed8c0ad
Lizzy Yao <lizzyyao11@gmail.com>
2015-07-20 00:25:55 -0400

    leapYear code modify

---
+++ DailyPractice/12_leapYearDefPractice.py
@@ -0,0 +1,25 @@
+#to get the year from user input and convert string to integer.
+def userInputYear():
+        year=raw_input("Please enter a year you would like to check: ")
+        year=int(year)
+        return year
+
+year=userInputYear()
+
+#evaluate input year is leap year or not.
+def checkLeapYear():
+    if year%4!=0 or (year%100==0 and year%400!=0):
+        return False    
+    else:
+        return True
+		
+LeapYear=checkLeapYear()
+
+#print evaluation result: is or not leap year.
+def printLeapYearResult():
+	if LeapYear is True:
+		print ("This year {} is leap year.".format(year))
+	else:
+		print("This year {} is not a leap year.".format(year))
+	
+printLeapYearResult()  
