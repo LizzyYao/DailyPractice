0c2c71d1d473ce0350a97b9cb4ab237108cda641
LizzyYao <lizzyyao11@gmail.com>
2015-07-26 22:30:18 -0400

    Update SQLQuery2.sql

--- SQLPractice/SQLQuery2.sql
+++ SQLPractice/SQLQuery2.sql
@@ -1,4 +1,4 @@
-# Show the CompanyName for all customers with an address in City 'Dallas'.
+-- Show the CompanyName for all customers with an address in City 'Dallas'.
 SELECT CompanyName
 FROM SalesLT.Customer
 WHERE CustomerID IN(SELECT CustomerID FROM SalesLT.CustomerAddress a RIGHT
