8fe77141ac2220767967edc7e8b89f485ba0a4d2
LizzyYao <lizzyyao11@gmail.com>
2015-10-13 00:03:52 -0400

    000

--- PythonPractice/13_findLargestNumber.py
+++ PythonPractice/13_findLargestNumber.py
@@ -1,37 +1,24 @@
-#find largest number in 3 numbers.
+#find largest number in 4 numbers.
 
-def largestNumber():
-	def getNumberFromUser(ordinal):
-	#"get the numbers from user and convert string to float numbers."
-		number=raw_input("please enter " + ordinal + " number: ")
-		number=float(number)
-		return number
 
-	def findLargestNumber(number1,number2,number3):
-	#evaluate each statement to find the largest number.
+def getNumberFromUser(ordinal):
+#"get the numbers from user and convert string to float numbers."
+	number=raw_input("please enter " + ordinal + " number: ")
+	number=float(number)
+	return number
 
-		if number1>=number2:
-			if number2>=number3:
-				return number1
-			else:
-				if number1>=number3:
-					return number1
-				return number3
-		else:
-			if number1>number3:
-				return number2
-			else:
-				if number2>number3:
-					return number2
-				return number3
-		
-		
+def findLargestNumber(number1,number2,number3, number4):
+#evaluate each statement to find the largest number.
 
-	number1=getNumberFromUser("first")
-	number2=getNumberFromUser("second")
-	number3=getNumberFromUser("third")
-	print findLargestNumber(number1,number2,number3)
-largestNumber()
+	return max(number1,number2,number3, number4)
+	
+	
+
+number1=getNumberFromUser("first")
+number2=getNumberFromUser("second")
+number3=getNumberFromUser("third")
+number4=getNumberFromUser("forth")
+print findLargestNumber(number1,number2,number3, number4)
 
 
 
